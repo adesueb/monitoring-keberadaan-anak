6388c2812691d23744a446cbcfc31dd48c58a828
ade <ade@ade>
2012-10-03 17:31:13 +0700

    koneksi

--- src/org/ade/monitoring/keberadaan/koneksi/KoneksiOrtuKeAnak.java
+++ src/org/ade/monitoring/keberadaan/koneksi/KoneksiOrtuKeAnak.java
@@ -1,16 +1,49 @@
 package org.ade.monitoring.keberadaan.koneksi;
 
 import org.ade.monitoring.keberadaan.entity.DataMonitoring;
+import org.ade.monitoring.keberadaan.entity.PesanData;
+
+import android.content.Context;
 
 public class KoneksiOrtuKeAnak {
 	
-	public KoneksiOrtuKeAnak(){
-		
+	public KoneksiOrtuKeAnak(Context context){
+		koneksiSms = new KoneksiSMSMonitoring(context);
+		koneksiInternet = new KoneksiInternetMonitoring();
 	}
 	
 	public boolean sendDataMonitoring(DataMonitoring dataMonitoring){
 		//TODO : send data monitoring sebelum disimpen di database ortu....
 		return false;
 	}
-	private KoneksiSMS koneksiSms;
+	
+	
+	
+	private final KoneksiInternetMonitoring koneksiInternet;
+	private final KoneksiSMSMonitoring koneksiSms;
+	
+	// kelas tambahan....................................................
+	
+	private final static class KoneksiSMSMonitoring extends KoneksiSMS{
+
+		public KoneksiSMSMonitoring(Context context) {
+			super(context);
+		}
+
+		@Override
+		public void menerimaPesanData(PesanData pesanData) {
+			
+		}
+		
+	}
+	
+	private final static class KoneksiInternetMonitoring extends KoneksiInternet{
+
+		@Override
+		public void menerimaPesanData(PesanData pesanData) {
+			
+		}
+		
+	}	
+	//.............................................................
 }
