e13cf354938f6c17054a5f86675842351a2f983e
catfiz <catfiz@catfiz-H67M-D2-B3>
2012-12-21 19:28:43 +0700

    laporan factory

--- src/org/ade/monitoring/keberadaan/util/LaporanFactory.java
+++ src/org/ade/monitoring/keberadaan/util/LaporanFactory.java
@@ -1,5 +1,10 @@
 package org.ade.monitoring.keberadaan.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.ade.monitoring.keberadaan.Variable.Status;
+import org.ade.monitoring.keberadaan.Variable.TipePesanData;
 import org.ade.monitoring.keberadaan.entity.Anak;
 import org.ade.monitoring.keberadaan.entity.DataMonitoring;
 import org.ade.monitoring.keberadaan.entity.Laporan;
@@ -7,14 +12,41 @@ import org.ade.monitoring.keberadaan.entity.Pelanggaran;
 
 public class LaporanFactory {
 
-	public static Laporan createLaporanFromPelanggaranAnak(Pelanggaran pelanggaran){
+	public static Laporan createLaporanPelanggaranAnak(List<Anak> anaks){
+		List<String> namaAnaks  =new ArrayList<String>();
+		List<Integer> values = new ArrayList<Integer>();
+		List<String> series = new ArrayList<String>();
+		
 		Laporan laporan = null;
+		
+		for(Anak anak:anaks){
+			namaAnaks.add(anak.getNamaAnak());
+			values.add(anak.getPelanggarans().size());
+			series.add("");
+		}
+		laporan.setValues(values);
+		laporan.setCategories(namaAnaks);
+		laporan.setSeries(null);
 		
 		return laporan;
 	}
 	
-	public static Laporan createLaporanFromPelanggaranSeharusnya(Pelanggaran pelanggaran){
+	public static Laporan createLaporanPelanggaranSeharusnya(List<Anak> anaks){
+		List<String> namaAnaks  =new ArrayList<String>();
+		List<Integer> values = new ArrayList<Integer>();
+		List<String> series = new ArrayList<String>();
+		
 		Laporan laporan = null;
+		
+		for(Anak anak:anaks){
+			namaAnaks.add(anak.getNamaAnak());
+			values.add(anak.getPelanggarans().size());
+			series.add("");
+		}
+		laporan.setValues(values);
+		laporan.setCategories(namaAnaks);
+		laporan.setSeries(null);
+		
 		
 		return laporan;
 	}
